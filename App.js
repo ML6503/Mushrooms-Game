3461f8180b81fd337e2d191094865b3e93fcb7ad
ML6503 <ml6503@gmail.com>
2020-02-15 00:18:53 +0200

    creatring repo

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>Go Mushrooming!</Text>
+      <Image
+        style={{width: 150, height: 150}}
+        source={require('./src/images/mukhomor.png')}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
