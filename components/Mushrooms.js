b3b064c740ffbbc4fdc0e807f288859f2f8d297d
ML6503 <ml6503@gmail.com>
2020-03-02 21:54:00 +0200

    adding images by const

--- components/Mushrooms.js
+++ components/Mushrooms.js
@@ -9,37 +9,58 @@ function getMushrooms() {
     m.forEach((key) => mushroomField[key] = mushrooms[key]);
     return mushroomField;
 }
+const images = {
+    mukhomor: require('../assets/images/mukhomor.png'),
+    poganka: require('../assets/images/blednayaPoganka.png'),
+    zelenushka: require('../assets/images/zelenushka.png'),
+    ryzhik: require('../assets/images/ryzhik.png'),
+};
 
 const Mushrooms = () => {
     console.log(getMushrooms());
-
+    //const grib = 'mukhomor';
+    // const mukhomorImg =((Object.values(getMushrooms()).find(m => m.id === grib)).img).valueOf();
+    const mukhomorImg = require('../assets/images/mukhomor.png');
+    
+    //  const mukhomor = valueOf((getMushrooms().find(m => m.id === grib)).img);
+    //  console.log(mukhomor);
     return (
+
         <View style={styles.field}>
             <View style={styles.mushroom}>
                 <Image
                     style={styles.mushroomImg}
-                    source={require('../assets/images/mukhomor.png')}
-                />
-            </View>
-            <View style={styles.mushroom}>
-                <Image
-                    style={styles.mushroomImg}
-                    source={require('../assets/images/blednayaPoganka.png')}
-                />
-            </View>
-            <View style={styles.mushroom}>
-                <Image
-                    style={styles.mushroomImg}
-                    source={require('../assets/images/ryzhik.png')}
-                />
-            </View>
-            <View style={styles.mushroom}>
-                <Image
-                    style={styles.mushroomImg}
-                    source={require('../assets/images/zelenushka.png')}
+                    source={mukhomorImg}
                 />
             </View>
         </View>
+
+    // <View style={styles.field}>
+    //     <View style={styles.mushroom}>
+    //         <Image
+    //             style={styles.mushroomImg}
+    //             source={require('../assets/images/mukhomor.png')}
+    //         />
+    //     </View>
+    //     <View style={styles.mushroom}>
+    //         <Image
+    //             style={styles.mushroomImg}
+    //             source={require('../assets/images/blednayaPoganka.png')}
+    //         />
+    //     </View>
+    //     <View style={styles.mushroom}>
+    //         <Image
+    //             style={styles.mushroomImg}
+    //             source={require('../assets/images/ryzhik.png')}
+    //         />
+    //     </View>
+    //     <View style={styles.mushroom}>
+    //         <Image
+    //             style={styles.mushroomImg}
+    //             source={require('../assets/images/zelenushka.png')}
+    //         />
+    //     </View>
+    // </View>
     )
 };
 
