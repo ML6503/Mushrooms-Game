3c681a5e147c8fc87785b2b8cf6ddd113c83b0a0
ML6503 <ml6503@gmail.com>
2020-03-02 20:07:29 +0200

    eslint fix

--- App.js
+++ App.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import Mushrooms from './components/Mushrooms';
 
 export default function App() {
-  return (
-    <ImageBackground source={require('./assets/images/forest.png')} style={styles.backgroundImage} > 
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>Go Mushrooming!</Text>
-      <Mushrooms />
-      </View>
-    </ImageBackground>
-  );
+    return (
+        <ImageBackground source={require('./assets/images/forest.png')} style={styles.backgroundImage} > 
+            <View style={styles.container}>
+                <Text style={styles.paragraph}>Go Mushrooming!</Text>
+                <Mushrooms />
+            </View>
+        </ImageBackground>
+    );
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-    flexDirection: 'row',
-    height: '100%',
-    resizeMode: 'stretch', // or 'cover'
-    width: '100%',     
-  },
-  container: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 24,
-    marginLeft: 100,
-  },
-  paragraph: {
-    margin: 24,
-    marginTop: 50,
-    fontSize: 50,
-    fontStyle:'italic',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: 'white',
-    textShadowRadius: 10,
-    textShadowColor: 'black',
-  }
+    backgroundImage: {
+        flex: 1,
+        flexDirection: 'row',
+        height: '100%',
+        resizeMode: 'stretch', // or 'cover'
+        width: '100%',     
+    },
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 24,
+        marginLeft: 100,
+    },
+    paragraph: {
+        margin: 24,
+        marginTop: 50,
+        fontSize: 50,
+        fontStyle:'italic',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'white',
+        textShadowRadius: 10,
+        textShadowColor: 'black',
+    }
 });
--- components/Mushrooms.js
+++ components/Mushrooms.js
@@ -2,53 +2,53 @@ import React from 'react';
 import { StyleSheet, View, Image, } from 'react-native';
 
 const Mushrooms = () => {
-  return (
-<View style={styles.field}>
-        <View style={styles.mushroom}>
-          <Image
-            style={styles.mushroomImg}
-            source={require('../assets/images/mukhomor.png')}
-            />
-       </View>
-       <View style={styles.mushroom}>
-          <Image
-          style={styles.mushroomImg}
-          source={require('../assets/images/blednayaPoganka.png')}
-          />
-         </View>
-         <View style={styles.mushroom}>
-           <Image
-          style={styles.mushroomImg}
-          source={require('../assets/images/ryzhik.png')}
-          />
+    return (
+        <View style={styles.field}>
+            <View style={styles.mushroom}>
+                <Image
+                    style={styles.mushroomImg}
+                    source={require('../assets/images/mukhomor.png')}
+                />
+            </View>
+            <View style={styles.mushroom}>
+                <Image
+                    style={styles.mushroomImg}
+                    source={require('../assets/images/blednayaPoganka.png')}
+                />
+            </View>
+            <View style={styles.mushroom}>
+                <Image
+                    style={styles.mushroomImg}
+                    source={require('../assets/images/ryzhik.png')}
+                />
+            </View>
+            <View style={styles.mushroom}>
+                <Image
+                    style={styles.mushroomImg}
+                    source={require('../assets/images/zelenushka.png')}
+                />
+            </View>
         </View>
-        <View style={styles.mushroom}>
-          <Image
-          style={styles.mushroomImg}
-          source={require('../assets/images/zelenushka.png')}
-          />
-        </View>
-      </View>
-)
+    )
 };
 
 const styles = StyleSheet.create({
     mushroomImg: {
-      height: 150,
-      width: 150,
-      resizeMode: "contain",
+        height: 150,
+        width: 150,
+        resizeMode: "contain",
     },
     mushroom: {
-      flex:1,    
-      justifyContent: 'space-between',
-      padding: 10,
-      alignItems:'center',    
+        flex:1,    
+        justifyContent: 'space-between',
+        padding: 10,
+        alignItems:'center',    
     },
     field: {
-      // flex: 1,
-      // flexDirection: 'row',
+        // flex: 1,
+        // flexDirection: 'row',
     },
-  });
+});
 
 export default Mushrooms;
     
